81fe1fc51eb59aa1f40c3ecf2d0a0d4eb4014423
Benjamin Friedman <friedman.benjamin@gmail.com>
2021-03-17 16:49:56 -0700

    fixes the parser

--- grammar.js
+++ grammar.js
@@ -1,9 +1,13 @@
 module.exports = grammar({
-  name: 'BoGL',
+  name: 'bogl',
 
   extras: $ => [
     $.comment,
     /[\n\s\uFEFF\u2060\u200B\u00A0\t]/
+  ],
+
+  conflicts: $ => [
+    [$.equation]
   ],
 
   word: $ => $.identifier,
@@ -13,69 +17,48 @@ module.exports = grammar({
     // recognize identifiers and types as known words
     //word: $ => $.identifier,
 
-    // TODO: add the actual grammar rules
-    source_file: $ => repeat($._definition),
+    source_file: $ => $.game,
 
-    _definition: $ => choice (
-      $.game_definition,
-      $.board_definition,
-      $.function_definition,
-      $.type_definition,
-      $.let_expr
+    // single & multi line comments
+    comment: $ => choice(
+      seq('--',/.*/),
+      seq('{-',/([^-]|-[^}])+/,'-}')
+    ),
+
+    // game definition
+    game: $ => seq(
+      $.game_def,
+      repeat($.type_def),
+      repeat($.value_def)
     ),
 
     // defining a game
-    game_definition: $ => seq(
+    game_def: $ => seq(
       'game',
       $.upperIdentifier
     ),
 
-    // defining a board
-    board_definition: $ => prec.left(3, seq(
-      $.identifier,
-      ':',
-      'Board',
-      repeat1($.board_pattern)
-    )),
+    // type definition
+    type_def: $ => choice(
+      $.gen_type_def,
+      $.board_type_def
+    ),
 
-    // defining a function with Input & Output
-    function_definition: $ => prec.left(2,seq(
-      $.identifier,
-      ':',
-      $._type,
-      optional(seq('->',$._type)), // optional input -> output
-      $.pattern
-    )),
-
-    // defining a type
-    type_definition: $ => seq(
+    // general type definition
+    gen_type_def: $ => seq(
       'type',
       $.upperIdentifier,
       '=',
-      $._type
+      $.type
     ),
 
-    comment: $ => seq('--',/.*/),
-
-    // BoGL types
-    _type: $ => choice (
-      $.builtin_type,
-      $.user_defined_type
-    ),
-
-    // builtin types
-    builtin_type: $ => prec(2,choice(
-      'Input',
-      'Bool',
-      'Int',
+    // board type definition
+    board_type_def: $ => seq(
+      'type',
       'Board',
-      $.array_type,
-      'Input',
-      'Player'
-    )),
-
-    // user defined types
-    user_defined_type: $ => prec(1,$.upperIdentifier),
+      '=',
+      $.array_type
+    ),
 
     array_type: $ => seq(
       'Array',
@@ -85,91 +68,209 @@ module.exports = grammar({
       $.number,
       ')',
       'of',
-      $._type
+      $.type
     ),
 
-    // pattern without parameters
-    pattern: $ => seq(
-      $.identifier,
-      optional($.parameter_list),
-      '=',
-      $._expression
+    // base type
+    btype : $ => choice(
+      $.builtin_type,
+      $.upperIdentifier,
+      $.tuple_type
     ),
 
-    // board pattern
-    board_pattern: $ => seq(
-      $.identifier,
-      '!',
-      $.position_list,
-      '=',
-      $._expression
-    ),
-
-    // position list for board equations
-    position_list: $ => seq(
+    // tuple types
+    tuple_type: $ => seq(
       '(',
-      choice(
-        seq($.identifier, ',', $.identifier),
-        seq($.number, ',', $.identifier),
-        seq($.identifier, ',', $.number),
-        seq($.number, ',', $.number),
+      seq($.btype, repeat1(seq(',', $.btype))),
+      ')'
+    ),
+
+    // enumeration type
+    etype: $ => seq(
+      '{',
+      seq($.upperIdentifier, repeat(seq(',', $.upperIdentifier))),
+      '}'
+    ),
+
+    // builtin types
+    builtin_type: $ => choice(
+      'Input',
+      'Bool',
+      'Int',
+      'Board',
+      'Input',
+      'Player',
+      'Content'
+    ),
+
+    // type expression
+    type: $ => choice(
+      $.btype,
+      $.etype,
+      seq($.type, '&', $.builtin_type),
+      seq($.type, '&', $.etype)
+    ),
+
+    // function type
+    ftype: $ => seq(
+      $.btype,
+      '->',
+      $.btype
+    ),
+
+    // value type
+    vtype: $ => choice(
+      $.btype,
+      $.ftype
+    ),
+
+    // value definition
+    value_def: $ => seq(
+      $.signature,
+      $.equation
+    ),
+
+    // type signature
+    signature: $ => seq(
+      $.identifier,
+      ':',
+      $.vtype
+    ),
+
+    // value equation
+    equation: $ => choice(
+      repeat1($.board_eq),
+      seq($.identifier,'=',$.expr),
+      seq($.identifier,$.parameter_list,'=',$.expr)
+    ),
+
+    // board equation
+    board_eq: $ => seq(
+        $.identifier,
+        '!',
+        '(',
+        $.pos,
+        ',',
+        $.pos,
+        ')',
+        '=',
+        $.expr
+    ),
+
+    // board position
+    pos: $ => choice(
+      $.number,
+      $.identifier
+    ),
+
+    // parameter listing for pattern matching
+    parameter_list: $ => prec(1,seq(
+      '(',
+      seq($.identifier, repeat(seq(',', $.identifier))),
+      ')'
+    )),
+
+    // value, name
+    aexpr: $ => choice(
+      $.number,
+      $.bool,
+      $.upperIdentifier,
+      $.identifier
+    ),
+
+    // tuples
+    tuple: $ => seq(
+      '(',
+      $.expr,
+      repeat1(
+        seq(
+          ',',
+          $.expr
+        )
       ),
       ')'
     ),
 
-    // parameter listing for pattern matching
-    parameter_list: $ => seq(
+    // function arguments
+    args: $ => prec(1,seq(
       '(',
-      seq($.identifier, repeat(seq(',', $.identifier))),
+      seq($.expr, repeat(seq(',', $.expr))),
       ')'
+    )),
+
+    // atomic expressions
+    expr: $ => choice(
+      $._expr,
+      seq('(',$._expr,')')  // parenthesized expressions
     ),
 
-    // types of valid BoGL expressions
-    _expression: $ => choice(
-      $.binary_expr,
-      $.identifier,
-      $.number,
-      $.bool,
+    _expr: $ => choice(
+      $.aexpr,
+      $.tuple,
+      $.app,
+      $.infix_app,
+      $.single_tuple_proj,
+      $.tuple_proj,
       $.let_expr,
-      $.while_do,
-      $.if_then_else,
-      $.unary_expr
+      $.conditional,
+      $.while_loop
     ),
+
+    // function application
+    app: $ => seq($.identifier,$.args),
+
+    // infix application
+    infix_app: $ => prec.left(2,seq($.expr,$.binop,$.expr)),
+
+    // single tuple projection
+    single_tuple_proj: $ => prec.left(4,seq($.expr,'#',$.number)),
+
+    // tuple projection
+    tuple_proj: $ => prec.left(4,seq($.expr,'#','(',$.number,repeat1(seq(',',$.number)),')')),
 
     // starts with a lowercase letter
     identifier: $ => /([a-z])|([a-z][a-zA-Z0-9_]+)/,
 
     // starts with an uppercase letter
     upperIdentifier: $ => /([A-Z])|([A-Z][a-zA-Z0-9_]+)/,
-    number: $ => /\d+/, ///-?\d+(\.\d+)?/,
-    bool: $ => /True|False/,
+    number: $ => /(-?\d+)/,
+    bool: $ => /(True|False)/,
 
-    // let exprs
+    // local definition
     let_expr: $ => seq(
       'let',
       $.identifier,
       '=',
-      $._expression,
+      $.expr,
       'in',
-      $._expression
+      $.expr
     ),
 
-    while_do: $ => seq(
-      'while',
-      $._expression,
-      'do',
-      $._expression
-    ),
-
-    if_then_else: $ => seq(
+    // conditional
+    conditional: $ => seq(
       'if',
-      $._expression,
+      $.expr,
       'then',
-      $._expression,
+      $.expr,
       'else',
-      $._expression
+      $.expr
     ),
 
+    // while loop
+    while_loop: $ => seq(
+      'while',
+      $.expr,
+      'do',
+      $.expr
+    ),
+
+    binop: $ => choice(
+      '+','-','*','/','!','==','/=','>','>=','<','<='
+    ),
+
+    break: $ => /\n|\r\n/
+
+    /*
     binary_expr: $ => choice(
       prec.left(1, seq($._expression, '>=', $._expression)),
       prec.left(1, seq($._expression, '>', $._expression)),
@@ -185,9 +286,12 @@ module.exports = grammar({
       prec.left(3, seq($._expression, '/', $._expression)),
       prec.left(3, seq($._expression, '%', $._expression)),
     ),
+    */
 
+    /*
     unary_expr: $ => choice(
       prec.left(2, seq('-', $._expression))
     ),
+    */
   }
 });
